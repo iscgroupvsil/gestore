47182dfc93dc588672dccbb1e099ecd95e9a0825
vito <vito@10.0.1.24>
2012-05-08 16:08:46 +0200

    ok

---
+++ gestorepec/src/main/java/gestorepec/gestorepec/App.java
@@ -0,0 +1,13 @@
+package gestorepec.gestorepec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
